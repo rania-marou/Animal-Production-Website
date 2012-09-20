bd7aaa647d8182d46d019e778bede009572af1be
rania-marou <rania.marou@gmail.com>
2012-09-20 17:58:34 +0300

    social-pages links added

---
+++ mobile/mobile_green_pal/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link href="mobilestyle.css" rel="stylesheet" type="text/css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Αρχική σελίδα</title>
+		<link rel="stylesheet" href="themes/ap_theme_2.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile.structure-1.1.1.min.css" />
+		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+	</head>
+	<body>
+		<div data-role="page" data-theme="a">
+			<div data-role="header" data-position="inline">
+				<h1>Αρχική σελίδα</h1>
+			</div>
+			<div data-role="content">	
+				<img class="logo" src="logo.png"></a>
+				<h3>Τμήμα Ζωικής Παραγωγής <br /> TEI Λάρισας</h3>	
+				<ul data-role="listview" data-inset="true" data-filter="false">
+					<li><a href="announces.php">Ανακοινώσεις</a></li>
+					<li><a href="teachers.php">Εκπαιδευτικοί</a></li>
+					<li><a href="telephones.php">Τηλέφωνα</a></li>
+					<li><a href="contact.php">Επικοινωνία</a></li>
+					<li><a href="links.php">Σύνδεσμοι</a></li>
+				</ul>
+				<h5>Προγραμματισμος-Σχεδίαση: <a href="mailto:rania.marou@gmail.com">Ράνια Μάρου</a></h5>
+				</div>
+<!--
+				
+			<div data-role="header" data-position="inline">
+				<h1>It Worked!</h1>
+			</div>
+			<div data-role="content" data-theme="a">
+				<p>Your theme was successfully downloaded. You can use this page as a reference for how to link it up!</p>
+				<pre>
+				<strong>&lt;link rel=&quot;stylesheet&quot; href=&quot;themes/ap_theme_2.min.css&quot; /&gt;</strong>
+				&lt;link rel=&quot;stylesheet&quot; href=&quot;http://code.jquery.com/mobile/1.1.1/jquery.mobile.structure-1.1.1.min.css&quot; /&gt;
+				&lt;script src=&quot;http://code.jquery.com/jquery-1.7.1.min.js&quot;&gt;&lt;/script&gt;
+				&lt;script src=&quot;http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js&quot;&gt;&lt;/script&gt;
+				</pre>
+				<p>This is content color swatch "A" and a preview of a <a href="#" class="ui-link">link</a>.</p>
+				<label for="slider1">Input slider:</label>
+				<input type="range" name="slider1" id="slider1" value="50" min="0" max="100" data-theme="a" />
+				<fieldset data-role="controlgroup"  data-type="horizontal" data-role="fieldcontain">
+				<legend>Cache settings:</legend>
+				<input type="radio" name="radio-choice-a1" id="radio-choice-a1" value="on" checked="checked" />
+				<label for="radio-choice-a1">On</label>
+				<input type="radio" name="radio-choice-a1" id="radio-choice-b1" value="off"  />
+				<label for="radio-choice-b1">Off</label>
+				</fieldset>
+			</div>
+-->
+		</div>
+	</body>
+</html>
